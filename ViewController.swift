02018c80caf6f122e1926523bfbe569c39626124
Westley Bonack <westley.bonack@gmail.com>
2015-10-05 13:42:07 -0500

    Second Commit

---
+++ ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  hw1-cs407
+//
+//  Created by Westley Bonack on 9/27/15.
+//  Copyright (c) 2015 Daniel Meyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+   // @IBOutlet weak var textField1: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var DestViewController : ViewThree = segue.destinationViewController as! ViewThree
+        
+        DestViewController.label1.text = textField1.text
+    }*/
+
+}
+
